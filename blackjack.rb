66c7548935c7ebbb9ece2945723e410e66edc658
George Toothman <george.toothman@gmail.com>
2014-06-17 17:34:44 -0400

    addded pseudo code for blackjack

--- blackjack.rb
+++ blackjack.rb
@@ -41,3 +41,51 @@
 # 		"Puts Dealer busts - you win"
 # 	else higher_value_wins
 # end
+
+# card_deck contains many cards 
+# cards contain suits
+# suits contain (diamonds, hearts, spades, and clubs)
+# each suit has cards with values 1..9 and the face cards plus an ace
+# the shoe_box contains four card_decks
+# computer randomly orders shoe_box
+# computer assigns two cards to player and to dealer
+# cards assigned to dealer are dealer_hand
+# cards assigned to player are player_hand
+# dealer reveals one card
+# player types 'hit' or 'stay'
+# if 'hit' computer assigns player one additional card
+# if 'stay' computer assigns dealer one additional card
+# while loop calculates the value of player_hand and dealer_hand
+# blackjack is met when player or dealer reach value of 21
+
+# table is initialized
+# => add four card_decks to shoe_box
+# => shuffle shoe_box
+# => assign player1 to player_hand
+# => assign player2 to dealer_hand
+# => add two cards from shoe_box to player_hand and dealer_hand
+
+# loop until theres a winner or bust
+# => score checks if 21 or bust is met 
+# => player_picks_actions
+# => if 'hit', computer_deals_card_to_player(shoe_box) into player_hand
+# => if 'pass', computer enters dealer_condition
+# => in dealer_condition dealer_hand value must be 17 else 
+## => computer_deals_card_to_dealer(shoe_box)
+
+
+# def initialize_table
+# 	t = {}
+# 	(1..4).each {|position| t[position] = ' '}
+# 	t
+# end
+
+suits = ['hearts', 'diamonds', 'clubs', 'spades']
+
+puts suits[0]
+puts suits[1]
+puts suits[2]
+puts suits[3]
+
+
+
---
+++ blackjack_practice.rb
@@ -0,0 +1,20 @@
+# blackjack_practice.rb
+
+score = 0
+while score < 21
+	score = sum of dealer_hand
+else bust
+end
+
+def check_winner
+	winning_score = 21
+	winning_score.each do calculates 
+										 if dealer_hand && player_hand ? "are exactly 21" : "are not 21"
+end
+
+begin
+	computer_deals_card_to_player(shoe_box)
+	player_status(action)
+	computer_deals_card_to_dealer(shoe_box)
+	winner = check_winner(dealer_hand) || check_winner(player_hand)
+end until winner
